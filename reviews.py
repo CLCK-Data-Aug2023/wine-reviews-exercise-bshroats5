f1bcb4e03bd70b8c256b8197c94f954d7c7711e1
Bret Shroats <bshroats5@gmail.com>
2023-12-10 14:59:49 -0500

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -3,3 +3,7 @@
 import pandas as pd
 import zipfile
 
+# Read the zip file
+with zipfile.ZipFile('data/winemag-data-130k-v2.csv.zip', 'r') as zip_ref:
+   df = pd.read_csv(zip_ref.open('winemag-data-130k-v2.csv'))
+
