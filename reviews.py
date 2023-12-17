88c3f1787651b49339d2fae6b8aaeb6c5ebeb3aa
Bret Shroats <bshroats5@gmail.com>
2023-12-17 14:12:41 -0500

    Updating CSV

--- reviews.py
+++ reviews.py
@@ -1,17 +1,10 @@
-# add your code here
-# Import Pandas and zip file
 import pandas as pd
 import zipfile
 
-# Read the zip file
-with zipfile.ZipFile('data/winemag-data-130k-v2.csv.zip', 'r') as zip_ref:
-   df = pd.read_csv(zip_ref.open('winemag-data-130k-v2.csv'))
-
-# Group the data by country & calculate the number of reviews and the average points for each country
-summary = df.groupby('country')['points'].agg(['count', 'mean']).rename(columns={'count': 'reviews', 'mean': 'points'})
-
-# Round the average points to 1 decimal point
+with zipfile.ZipFile('data/winemag-data-130k-v2.csv.zip', 'r') as z:
+   with z.open('winemag-data-130k-v2.csv') as f:
+       df = pd.read_csv(f)
+summary = df.groupby('country').agg({'points': ['count', 'mean']})
+summary.columns = ['count', 'points']
 summary['points'] = summary['points'].round(1)
-
-# Write the summary data to a new CSV file.
 summary.to_csv('data/reviews-per-country.csv', index=True)
